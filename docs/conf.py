8167283aba2e593e930df0a45dd839aee2618c28
Leon Stafford <me@ljs.dev>
2020-08-21 03:19:40 +0930

    adjust config

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = "sphinx_rtd_theme"
 
+html_theme_options = {
+    # 'canonical_url': '',
+    # 'analytics_id': '',
+    # 'logo_only': False,
+    'display_version': False,
+    # 'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': '',
+    # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
